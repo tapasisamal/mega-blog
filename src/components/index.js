071e96e641c636fdf658b9a390cec2ed02b54aab
tapasisamal <tapasisamal3@gmail.com>
2026-04-02 19:53:59 +0530

    fix: resolve casing issues for Input and PostForm components

--- src/components/index.js
+++ src/components/index.js
@@ -4,12 +4,12 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
-import Input from "./temp";
+import Input from "./Input";
 import Select from "./Select";
 import RTE from "./RTE";
 import Singup from "./Signup";
 import Login from "./Login";
-import PostForm from "./post-form/temp2";
+import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
 
